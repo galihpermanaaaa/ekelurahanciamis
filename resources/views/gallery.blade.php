64d51f9576bbfceee2e437e78ebe2f01701cf669
Asep-SR <luckyhermawanroza@gmail.com>
2022-03-22 00:23:07 +0700

    update gallery

---
+++ resources/views/gallery.blade.php
@@ -0,0 +1,67 @@
+
+@include('layouts.topbarlanding')
+
+  <main id="main">
+
+    <!-- ======= Breadcrumbs ======= -->
+    <section id="breadcrumbs" class="breadcrumbs">
+      <div class="container">
+
+        <ol>
+          <li><a href="{{ route('index') }}">Home</a></li>
+          <li>Gallery</li>
+        </ol>
+        <h2>Gallery Kegiatan</h2>
+
+      </div>
+    </section><!-- End Breadcrumbs -->
+
+    <section id="about" class="about">
+      <div class="container" data-aos="fade-up">
+
+        <script src="https://apps.elfsight.com/p/platform.js" defer></script>
+<div class="elfsight-app-3a8f02d4-a9a4-4ead-8acf-41e825c8e416"></div>
+
+      </div>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+    </section>
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+    <div class="container footer-bottom clearfix">
+      <div class="copyright">
+      &copy; Copyright <strong><span>Kelurahan Ciamis 2022</span></strong>. All Rights Reserved
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+
+  <a href="#" class="back-to-top"><i class="ri-arrow-up-line"></i></a>
+  <div id="preloader"></div>
+
+  <!-- Vendor JS Files -->
+  <script src="http://cdn.bootcss.com/jquery/2.2.4/jquery.min.js"></script>
+        <script src="http://cdn.bootcss.com/toastr.js/latest/js/toastr.min.js"></script>
+  <script src="{{ URL::to('Arsha/vendor/jquery/jquery.min.js') }}"></script>
+  <script src="{{ URL::to('Arsha/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+  <script src="{{ URL::to('Arsha/vendor/jquery.easing/jquery.easing.min.js') }}"></script>
+  <!--  -->
+  <script src="{{ URL::to('Arsha/vendor/waypoints/jquery.waypoints.min.js') }}"></script>
+  <script src="{{ URL::to('Arsha/vendor/isotope-layout/isotope.pkgd.min.js') }}"></script>
+  <script src="{{ URL::to('Arsha/vendor/venobox/venobox.min.js') }}"></script>
+  <script src="{{ URL::to('Arsha/vendor/owl.carousel/owl.carousel.min.js') }}"></script>
+  <script src="{{ URL::to('Arsha/vendor/aos/aos.js') }}"></script>
+
+  <script src="https://www.markuptag.com/bootstrap/5/js/bootstrap.bundle.min.js"></script>
+  <!-- Template Main JS File -->
+  <script src="{{ URL::to('Arsha/js/main.js') }}"></script>
+</body>
+
+</html>
