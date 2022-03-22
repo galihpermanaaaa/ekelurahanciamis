d4d3cbec872a12f71e8f7deacdc89c79b0341720
Asep-SR <luckyhermawanroza@gmail.com>
2022-03-22 14:11:27 +0700

    update

--- resources/views/layouts/topbarlanding.blade.php
+++ resources/views/layouts/topbarlanding.blade.php
@@ -61,6 +61,8 @@
   <link href="{{ URL::to('Arsha/css/style.css') }}" rel="stylesheet">
   <link rel="stylesheet" href="https://www.markuptag.com/bootstrap/5/css/bootstrap.min.css" />
   <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
  <meta name="viewport" content="width=device-width, initial-scale=1">
  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
